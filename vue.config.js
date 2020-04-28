73ae0ad304174db54fd3d9ba9f49efc58ca00fa8
link <link@linkdeMacBook-Pro.local>
2020-04-28 18:15:45 +0800

    项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+const path = require('path')
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
 module.exports = {
   chainWebpack: config => {
     config.resolve.alias.set('@', resolve('src'))
